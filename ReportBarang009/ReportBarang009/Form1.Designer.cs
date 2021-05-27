6b590f6727ea387e6f62c64f9c4942820169ebbb
wawawadadaw <45932666+wawawadadaw@users.noreply.github.com>
2021-05-27 08:19:09 +0700

    nambah form

---
+++ ReportBarang009/ReportBarang009/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace ReportBarang009
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnExcel = new System.Windows.Forms.Button();
+            this.btnPdf = new System.Windows.Forms.Button();
+            this.dgView = new System.Windows.Forms.DataGridView();
+            this.quizMahasiswaDataSet = new ReportBarang009.QuizMahasiswaDataSet();
+            this.tblbarangBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tbl_barangTableAdapter = new ReportBarang009.QuizMahasiswaDataSetTableAdapters.tbl_barangTableAdapter();
+            this.idbarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.namabarangDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hargaDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stokDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.namasupplierDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quizMahasiswaDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblbarangBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnExcel
+            // 
+            this.btnExcel.Location = new System.Drawing.Point(425, 121);
+            this.btnExcel.Name = "btnExcel";
+            this.btnExcel.Size = new System.Drawing.Size(116, 23);
+            this.btnExcel.TabIndex = 5;
+            this.btnExcel.Text = "Import Into Excel";
+            this.btnExcel.UseVisualStyleBackColor = true;
+            // 
+            // btnPdf
+            // 
+            this.btnPdf.Location = new System.Drawing.Point(206, 121);
+            this.btnPdf.Name = "btnPdf";
+            this.btnPdf.Size = new System.Drawing.Size(123, 23);
+            this.btnPdf.TabIndex = 4;
+            this.btnPdf.Text = "Export into PDF";
+            this.btnPdf.UseVisualStyleBackColor = true;
+            // 
+            // dgView
+            // 
+            this.dgView.AllowUserToAddRows = false;
+            this.dgView.AllowUserToDeleteRows = false;
+            this.dgView.AutoGenerateColumns = false;
+            this.dgView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idbarangDataGridViewTextBoxColumn,
+            this.namabarangDataGridViewTextBoxColumn,
+            this.hargaDataGridViewTextBoxColumn,
+            this.stokDataGridViewTextBoxColumn,
+            this.namasupplierDataGridViewTextBoxColumn});
+            this.dgView.DataSource = this.tblbarangBindingSource;
+            this.dgView.Location = new System.Drawing.Point(110, 185);
+            this.dgView.Name = "dgView";
+            this.dgView.ReadOnly = true;
+            this.dgView.Size = new System.Drawing.Size(542, 150);
+            this.dgView.TabIndex = 3;
+            // 
+            // quizMahasiswaDataSet
+            // 
+            this.quizMahasiswaDataSet.DataSetName = "QuizMahasiswaDataSet";
+            this.quizMahasiswaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblbarangBindingSource
+            // 
+            this.tblbarangBindingSource.DataMember = "tbl_barang";
+            this.tblbarangBindingSource.DataSource = this.quizMahasiswaDataSet;
+            // 
+            // tbl_barangTableAdapter
+            // 
+            this.tbl_barangTableAdapter.ClearBeforeFill = true;
+            // 
+            // idbarangDataGridViewTextBoxColumn
+            // 
+            this.idbarangDataGridViewTextBoxColumn.DataPropertyName = "id_barang";
+            this.idbarangDataGridViewTextBoxColumn.HeaderText = "id_barang";
+            this.idbarangDataGridViewTextBoxColumn.Name = "idbarangDataGridViewTextBoxColumn";
+            // 
+            // namabarangDataGridViewTextBoxColumn
+            // 
+            this.namabarangDataGridViewTextBoxColumn.DataPropertyName = "nama_barang";
+            this.namabarangDataGridViewTextBoxColumn.HeaderText = "nama_barang";
+            this.namabarangDataGridViewTextBoxColumn.Name = "namabarangDataGridViewTextBoxColumn";
+            // 
+            // hargaDataGridViewTextBoxColumn
+            // 
+            this.hargaDataGridViewTextBoxColumn.DataPropertyName = "harga";
+            this.hargaDataGridViewTextBoxColumn.HeaderText = "harga";
+            this.hargaDataGridViewTextBoxColumn.Name = "hargaDataGridViewTextBoxColumn";
+            // 
+            // stokDataGridViewTextBoxColumn
+            // 
+            this.stokDataGridViewTextBoxColumn.DataPropertyName = "stok";
+            this.stokDataGridViewTextBoxColumn.HeaderText = "stok";
+            this.stokDataGridViewTextBoxColumn.Name = "stokDataGridViewTextBoxColumn";
+            // 
+            // namasupplierDataGridViewTextBoxColumn
+            // 
+            this.namasupplierDataGridViewTextBoxColumn.DataPropertyName = "nama_supplier";
+            this.namasupplierDataGridViewTextBoxColumn.HeaderText = "nama_supplier";
+            this.namasupplierDataGridViewTextBoxColumn.Name = "namasupplierDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 450);
+            this.Controls.Add(this.btnExcel);
+            this.Controls.Add(this.btnPdf);
+            this.Controls.Add(this.dgView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.quizMahasiswaDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblbarangBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExcel;
+        private System.Windows.Forms.Button btnPdf;
+        private System.Windows.Forms.DataGridView dgView;
+        private QuizMahasiswaDataSet quizMahasiswaDataSet;
+        private System.Windows.Forms.BindingSource tblbarangBindingSource;
+        private QuizMahasiswaDataSetTableAdapters.tbl_barangTableAdapter tbl_barangTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idbarangDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn namabarangDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hargaDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stokDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn namasupplierDataGridViewTextBoxColumn;
+    }
+}
+
